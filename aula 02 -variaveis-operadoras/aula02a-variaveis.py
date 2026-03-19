9eb61b79c731eec50482d76a7dc3075d593ae3db
joaovitor <joaovitor12119@gmail.com>
2026-03-19 20:57:16 -0300

    aula 02 variaveis e operadores

---
+++ aula 02 -variaveis-operadoras/aula02a-variaveis.py
@@ -0,0 +1,39 @@
+print("ola mundo")
+print(7 + 4)
+print("7 + 4")
+print("7" + "4") # contenação de strings
+# comentarios de 1 linha
+'''
+comentarios de 
+multiplas 
+linhas 
+autor: joaovitor
+versao: 1.0.1
+'''
+
+# variaveis
+nome = "joaovitor" # str
+idade = 26 # int
+peso = 70.2 # float
+
+print(nome, idade, peso)
+
+
+print("oiiii,{nome}")
+
+# input - simulaçao de forms no cmd
+nome = input("digite o seu nome")
+idade = int( input("digite a idade"))
+peso = float (input("digite o peso"))
+
+print(nome, idade, peso)
+print(idade + 1)
+
+ano_nascimento = 1999
+ano_atual = 2026
+idade = ano_atual - ano_nascimento
+print(f"sua idade é: {idade}")
+
+
+
+
---
+++ aula 02 -variaveis-operadoras/desafio.py
@@ -0,0 +1,14 @@
+nome = input("qual seu nome?")
+print(f"boas vindas, {nome}")
+
+dia_nascimento = input("qual dia vc nasceu?")
+mês_nascimento = input("qual mês vc nasceu?")
+ano_nascimento = input ("qual ano vc nasceu?")
+
+print(f"{nome}, vc nasceu no dia {dia_nascimento} de {mês_nascimento} de {ano_nascimento}")
+
+
+num_1 = int(input("digite um numero"))
+num_2 = int(input("digite outro numero"))
+soma = num_1 + num_2
+print(f"o resultado é igual a: {soma}")
