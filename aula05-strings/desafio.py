0aec3de1815e3c0df902ded388d3aadfd65f9884
joaovitor12119 <joaovitor12119@gmail.com>
2026-04-30 20:51:12 -0300

    desafio

---
+++ aula05-strings/desafio.py
@@ -0,0 +1,6 @@
+nomes = ["joao", "jair", "Lucas", "neymar"]
+for i in range(len(nomes)):
+
+    for j in range(i+1, len(nomes)):
+        print(nomes[i], nomes[j])
+        
---
+++ aula05-strings/matrizes.py
@@ -0,0 +1,10 @@
+musicas = [
+    ["Baby", "Justin Bieber"],
+    ["Creep", "Radiohead",],
+    ["Mirros", "Justin Timberlake"]
+]
+
+for musica in musicas:
+    for info in musica:
+        print(info)
+        print()
